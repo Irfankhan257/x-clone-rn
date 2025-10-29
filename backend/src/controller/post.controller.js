13de35c099484d4d6f3669a6f68439137ce2d315
Irfan Khan <irfan1014@hulhub.com>
2025-10-29 11:11:31 +0500

    added get auth

--- backend/src/controller/post.controller.js
+++ backend/src/controller/post.controller.js
@@ -5,6 +5,7 @@ import cloudinary from "../config/cloudinary.js";
 import User from "../models/user.model.js";
 import Notification from "../models/notification.model.js";
 import Comment from "../models/comment.model.js";
+import { getAuth } from "@clerk/express";
 
 export const getPosts = asyncHandler(async (req, res) => {
   const posts = await Post.find()
